bd9a574f3a46e3c241bdff551e3752c13dc4ce09
2xelevate <misteats@gmail.com>
2024-08-13 07:58:38 -0600

    l

--- components/dashboard/signOut.tsx
+++ components/dashboard/signOut.tsx
@@ -1,4 +1,3 @@
-import { signOut } from "@/auth";
 import { Button } from "@/components/ui/button";
 
 export function SignOut() {
@@ -7,7 +6,6 @@ export function SignOut() {
       <form
         action={async () => {
           "use server";
-          await signOut();
         }}
       >
         <Button type="submit">Logout</Button>
